30727597d1fe6d670ed5693e9434d6360a060137
VictorBK <victorbrian4@gmail.com>
2023-08-11 10:09:07 +0300

    Terminal-related change

--- index.js
+++ index.js
@@ -1,5 +1,21 @@
 const express = require("express");
 const app = express();
+const pool = require("./db");
+
+app.use(express.json()) // -> req.body
+
+
+//ROUTES//
+
+//get all todos
+
+//get a todo
+
+//create a todo
+
+//update a todo
+
+//delete a todo
 
 app.listen(5000, () => {
     console.log("server is listening on port 5000");
